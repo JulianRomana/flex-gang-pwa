6f964ebeea1dd195c11d76a1333d68bf6d24a9ba
JulianRomana <jjulian.romana@gmail.com>
2020-05-18 16:22:04 +0200

    feat(icons): add icons

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,17 @@
 module.exports = {
   pwa: {
-    name: 'FLEX GANG PWA',
+    name: 'fighting-games',
     themeColor: '#4DBA87',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
     manifestOptions: {
       display: 'fullscreen',
+    },
+    iconsPath: {
+      favicon32: '/public/img/icons/favicon.ico',
+      appleTouchIcon: '/public/img/icons/icon-152x152.png',
+      msTileImage: '/public/img/icons/icon-144x144.png',
     },
   },
 }
