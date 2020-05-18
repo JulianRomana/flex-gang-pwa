06bf0355993f54ff1e5fa2e7e26ee12240a3ecaa
JulianRomana <jjulian.romana@gmail.com>
2020-05-18 10:25:50 +0200

    add pwa config

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  pwa: {
+    name: 'FLEX GANG PWA',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      display: 'fullscreen',
+    },
+  },
+}
