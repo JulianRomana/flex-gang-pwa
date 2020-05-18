9f36982a14814a56612ba27f57c39a4d45e1ea31
simtaxx <souici.haris@gmail.com>
2020-05-18 15:59:28 +0200

    ArticlePreview - Create a module for article preview and added it in Articles

---
+++ src/modules/ArticlePreview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <img class="picture" src="https://images.pexels.com/photos/616808/pexels-photo-616808.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="pciture">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ArticlePreview',
+}
+</script >
+
+<style module lang="scss">
+
+</style>
--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <h1>Articles</h1>
+    <ArticlePreview></ArticlePreview>
   </div>
 </template>
 
 <script>
+import ArticlePreview from '../modules/ArticlePreview.vue'
+
 export default {
   name: 'Articles',
+  components: {
+    ArticlePreview,
+  },
+  mounted() {
+    console.log(ArticlePreview)
+  },
 }
 </script>
 
