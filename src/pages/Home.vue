e49f05cc8d2167eafb54373482378a33ebce8a82
simtaxx <souici.haris@gmail.com>
2020-05-19 16:03:53 +0200

    Home - add good properties

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,19 +1,8 @@
 <template>
-  <div class="grid">
+  <div class="grid" v-if="articles">
     <router-link to="/articles">articles</router-link>
     <div v-for="article in articles.slice(0,3)" :key="article.id">
-      <ArticlePreview
-        picture="http://image.jeuxvideo.com/medias-md/157849/1578490854-2372-card.jpg"
-        :title="article.title"
-        date="18/05/2020"
-        author="Bill Gates"
-        time="3 mins"
-        :content="article.body"
-        :showContent="true"
-        :firstArticle="article"
-        :id="article.id"
-        >
-      </ArticlePreview>
+      <ArticlePreview :article="article" :showContent="true"></ArticlePreview>
     </div>
   </div>
 </template>
